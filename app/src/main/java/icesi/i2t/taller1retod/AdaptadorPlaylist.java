15176338b9f61d209701ac53b2f99747321a29a8
JuanEsteban <juanes-2504@hotmail.com>
2018-09-27 12:49:26 -0500

    sep 27 - 12:49 pm

---
+++ app/src/main/java/icesi/i2t/taller1retod/AdaptadorPlaylist.java
@@ -0,0 +1,64 @@
+package icesi.i2t.taller1retod;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.deezer.sdk.model.Playlist;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class AdaptadorPlaylist extends BaseAdapter {
+
+    private Activity activity;
+    private ArrayList<Playlist> arrayPlaylist;
+
+    public AdaptadorPlaylist(Activity activity){
+        this.activity = activity;
+        arrayPlaylist = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return arrayPlaylist.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = LayoutInflater.from(activity);
+        View viewPlay = inflater.inflate(R.layout.view_play, null, false);
+        // Inicializo componentes del view
+        TextView tv_nombre_lista = viewPlay.findViewById(R.id.tv_nombre_lista);
+        TextView tv_nombre_creador = viewPlay.findViewById(R.id.tv_nombre_creador);
+        TextView tv_numero_items = viewPlay.findViewById(R.id.tv_numero_items);
+        ImageView iv_image = viewPlay.findViewById(R.id.iv_image);
+        // Lleno los componentes del view
+        tv_nombre_lista.setText("Nombre Lista: "+arrayPlaylist.get(position).getTitle());
+        tv_nombre_lista.setText("Nombre Creador: "+arrayPlaylist.get(position).getCreator());
+        tv_nombre_lista.setText("Numero Items: "+arrayPlaylist.get(position));
+        Picasso.get().load(arrayPlaylist.get(position).getSmallImageUrl()).into(iv_image);
+
+        return viewPlay;
+    }
+
+    public void agregarPlaylist(Playlist playlist){
+        arrayPlaylist.add(playlist);
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/icesi/i2t/taller1retod/MainActivity.java
+++ app/src/main/java/icesi/i2t/taller1retod/MainActivity.java
@@ -3,18 +3,30 @@ package icesi.i2t.taller1retod;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
+import com.deezer.sdk.model.Album;
 import com.deezer.sdk.model.Permissions;
+import com.deezer.sdk.model.Playlist;
 import com.deezer.sdk.network.connect.DeezerConnect;
 import com.deezer.sdk.network.connect.SessionStore;
 import com.deezer.sdk.network.connect.event.DialogListener;
+import com.deezer.sdk.network.request.DeezerRequest;
+import com.deezer.sdk.network.request.DeezerRequestFactory;
+import com.deezer.sdk.network.request.event.JsonRequestListener;
+import com.deezer.sdk.network.request.event.RequestListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et_buscar_lista;
     private ImageButton ib_buscar;
+    private ListView lv_listas;
+    private AdaptadorPlaylist adaptadorPlaylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
         et_buscar_lista = findViewById(R.id.et_buscar_lista);
         ib_buscar = findViewById(R.id.ib_buscar);
+        lv_listas = findViewById(R.id.lv_listas);
+        adaptadorPlaylist = new AdaptadorPlaylist(this);
+        lv_listas.setAdapter(adaptadorPlaylist);
 
         String aplicationID = "301624";
         final DeezerConnect deezerConnect = new DeezerConnect(this, aplicationID);
@@ -51,6 +66,38 @@ public class MainActivity extends AppCompatActivity {
 
         // Launches the authentication process
         deezerConnect.authorize(this, permissions, listener);
+
+        ib_buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // the request listener
+                RequestListener listener = new JsonRequestListener() {
+
+                    public void onResult(Object result, Object requestId) {
+                        List<Playlist> ListPlay = (List<Playlist>) result;
+                        // do something with the albums
+                        for (int i=0;i<ListPlay.size();i++)
+                            adaptadorPlaylist.agregarPlaylist(ListPlay.get(i));
+                    }
+
+                    public void onUnparsedResult(String requestResponse, Object requestId) {
+                    }
+
+                    public void onException(Exception e, Object requestId) {
+                    }
+                };
+
+                // create the request
+                //long artistId = 11472;
+                //DeezerRequest request = DeezerRequestFactory.requestArtistAlbums(artistId);
+                DeezerRequest request = DeezerRequestFactory.requestSearchPlaylists(et_buscar_lista.getText().toString());
+                // set a requestId, that will be passed on the listener's callback methods
+                request.setId("myRequest");
+
+                // launch the request asynchronously
+                deezerConnect.requestAsync(request, listener);
+            }
+        });
 
     }
 }
